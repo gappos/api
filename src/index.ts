a89b2645f6fd7ae685639c2290f0931f33f2dfc2
Arturas Mickiewicz <arturasmckwcz@gmail.com>
2023-01-21 15:23:55 +0100

    cleanup src/index

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,7 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
-import { readFileSync } from 'fs';
 
-import resolvers from './graphql/resolvers';
-const typeDefs = readFileSync('./src/graphql/schema.graphql', {
-  encoding: 'utf-8'
-});
+import { resolvers, typeDefs } from './graphql';
 
 const server = new ApolloServer({
   typeDefs,
