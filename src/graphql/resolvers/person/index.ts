809a3e25ea1d99024e5f4c4832146c5b5512fa7a
Arturas Mickiewicz <arturasmckwcz@gmail.com>
2023-02-10 10:46:40 +0100

    added PersonInput instead of a bunch of primitives

--- src/graphql/resolvers/person/index.ts
+++ src/graphql/resolvers/person/index.ts
@@ -1,6 +1,6 @@
 import { Arg, Mutation, Query, Resolver } from 'type-graphql';
 
-import { Gender, Person } from '../../../models';
+import { Person, PersonInput } from '../../../models';
 import { getPersons, addPerson } from './helpers';
 
 @Resolver(() => Person)
@@ -12,26 +12,9 @@ export class PersonResolvers {
 
   @Mutation(() => Boolean)
   async addPerson(
-    @Arg('firstName') firstName: string,
-    @Arg('lastName') lastName: string,
-    @Arg('gender') gender: Gender,
-    @Arg('dob') dob: string,
-    @Arg('middleName', { nullable: true }) middleName?: string,
-    @Arg('placeId', { nullable: true }) placeId?: string,
-    @Arg('pobId', { nullable: true }) pobId?: string,
-    @Arg('dod', { nullable: true }) dod?: string,
+    @Arg('personAttributes', () => PersonInput) personAttributes: PersonInput,
   ): Promise<boolean> {
-    const person = {
-      firstName,
-      lastName,
-      middleName,
-      dob,
-      gender,
-      placeId,
-      pobId,
-      dod,
-    };
-    console.log('PersonResolvers:addPerson:\n', person);
-    return await addPerson(person);
+    console.log('PersonResolvers:addPerson:\n', personAttributes);
+    return await addPerson(personAttributes);
   }
 }
