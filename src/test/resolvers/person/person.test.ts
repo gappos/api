549bbca4ec9b5c0689f36817f7917302cb48c9fe
Arturas Mickiewicz <arturasmckwcz@gmail.com>
2023-02-17 15:31:52 +0100

    added resolver tests

---
+++ src/test/resolvers/person/person.test.ts
@@ -0,0 +1,63 @@
+import { expect } from 'expect';
+import { SinonStub, stub } from 'sinon';
+
+import { Gender, Person, PersonInput } from '../../../models';
+import { PersonResolvers } from '../../../graphql/resolvers/person';
+
+describe('PersonResolvers', () => {
+  let resolver: PersonResolvers;
+  let createStub: SinonStub;
+
+  describe('persons', () => {
+    before(() => {
+      resolver = new PersonResolvers();
+      createStub = stub(Person, 'findAll');
+    });
+
+    after(() => {
+      createStub.restore();
+    });
+
+    it('should return an array of persons', async () => {
+      const expectedPersons: unknown[] = [{}];
+      createStub.resolves(expectedPersons);
+
+      const actualPersons = await resolver.persons();
+
+      expect(actualPersons).toEqual(expectedPersons);
+    });
+  });
+
+  describe('addPerson', () => {
+    let result: unknown;
+    let personAttributes: PersonInput;
+
+    before(async () => {
+      resolver = new PersonResolvers();
+      createStub = stub(Person, 'create').resolves();
+
+      personAttributes = {
+        firstName: '',
+        lastName: '',
+        gender: Gender.FEMALE,
+        dob: new Date().toISOString(),
+      };
+
+      result = await resolver.addPerson(personAttributes);
+    });
+
+    after(() => {
+      createStub.restore();
+    });
+
+    it('should return true', async () => {
+      expect(result).toBeTruthy();
+    });
+
+    it('should be called with proper arg', async () => {
+      const calledAttributesProps = Object.keys(createStub.args[0][0]).sort();
+      const expectedAttributesProps = ['firstName', 'lastName', 'gender', 'dob', 'dod'].sort();
+      expect(calledAttributesProps).toEqual(expectedAttributesProps);
+    });
+  });
+});
