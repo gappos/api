d2101a9f2f04b8ab09f258890613b4f75580b39b
Arturas Mickiewicz <arturasmckwcz@gmail.com>
2023-02-18 21:59:54 +0100

    updated docker files

--- postgres/000-mocha-database.sh
+++ postgres/000-mocha-database.sh
@@ -2,9 +2,16 @@
 
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE USER mocha WITH PASSWORD mocha;
-    ALTER USER mocha WITH SUPERUSER;
-    CREATE DATABASE mocha;
-    GRANT ALL PRIVILEGES ON DATABASE mocha TO mocha;
-EOSQL
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -f /docker-entrypoint-initdb.d/init_mocha.sql
+# psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+#     DO $$ 
+#     BEGIN 
+#         CREATE USER mocha WITH PASSWORD 'mocha' SUPERUSER;
+#         EXCEPTION WHEN duplicate_object THEN RAISE NOTICE '% skipping', SQLERRM USING ERRCODE = SQLSTATE;
+#     END;
+#     $$;
+
+#     CREATE DATABASE IF NOT EXISTS mocha;
+#     GRANT ALL PRIVILEGES ON DATABASE mocha TO mocha;
+# EOSQL
+
