663eee77169f0db64ca72a7d1851c37e9bebf70a
Arturas Mickiewicz <arturasmckwcz@gmail.com>
2023-02-18 22:02:41 +0100

    init sequelize instance for tests and test connection to db

---
+++ src/test/utils/getTestSequelize.ts
@@ -0,0 +1,33 @@
+import { Sequelize, SequelizeOptions } from 'sequelize-typescript';
+import dotenv from 'dotenv';
+
+import { Book } from '../../models';
+import { Location } from '../../models';
+import { Person } from '../../models';
+
+dotenv.config();
+
+let sequelize: Sequelize;
+
+const getTestSequelize = async (): Promise<Sequelize> => {
+  if (sequelize) return sequelize;
+
+  const testUri = process.env.DB_CONNECTION_STRING_TEST as string;
+  if (!testUri) throw new Error("Connection to the test DB hasn't been set");
+
+  const sequelizeOptions: SequelizeOptions = {
+    logging: false,
+    dialect: 'postgres',
+  };
+  sequelize = new Sequelize(testUri, sequelizeOptions);
+
+  // Define your models here
+  sequelize.addModels([Book, Person, Location]);
+
+  // Sync the models with the database
+  await sequelize.sync();
+
+  return sequelize;
+};
+
+export default getTestSequelize;
