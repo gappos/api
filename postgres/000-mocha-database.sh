a71e8a5498a8e959e7fe82cc9c7edf0e664ef2cc
Arturas Mickiewicz <arturasmckwcz@gmail.com>
2023-02-21 11:56:38 +0100

    seems like the final fix

--- postgres/000-mocha-database.sh
+++ postgres/000-mocha-database.sh
@@ -5,11 +5,11 @@ set -e
 # psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" -f /docker-entrypoint-initdb.d/init_mocha.sql
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
   SELECT 'CREATE USER mocha WITH PASSWORD ''mocha'' SUPERUSER;'
-  WHERE NOT EXISTS (SELECT usename FROM "pg_catalog"."pg_user")\gexec
+  WHERE NOT EXISTS (SELECT usename FROM "pg_catalog"."pg_user" WHERE usename='mocha') \gexec
 
 
   SELECT 'CREATE DATABASE mocha;'
-  WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'mocha')\gexec
+  WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'mocha') \gexec
 
   GRANT ALL PRIVILEGES ON DATABASE mocha TO mocha;
 EOSQL
