b5433ab21aff9b8029ceb6d404fa9aab20404921
Arturas Mickiewicz <arturasmckwcz@gmail.com>
2023-03-14 18:03:22 +0200

    added field resolver tests for Person.spouses

--- src/test/resolvers/person/person.test.ts
+++ src/test/resolvers/person/person.test.ts
@@ -1,9 +1,15 @@
 import { expect } from 'expect';
 import { SinonStub, stub } from 'sinon';
 
-import { Gender, Person, PersonInput, Location } from '../../../models';
+import { Gender, Person, PersonInput, Location, Spouse } from '../../../models';
 import { PersonResolvers } from '../../../graphql/resolvers/person';
-import { createLocationForTest, createPersonForTest, getContextForTest } from '../../utils/utils';
+import * as personHelpers from '../../../graphql/resolvers/person/helpers';
+import {
+  createFamilyForTest,
+  createLocationForTest,
+  createPersonForTest,
+  getContextForTest,
+} from '../../utils/utils';
 
 describe('PersonResolvers', () => {
   let resolver: PersonResolvers;
@@ -92,26 +98,59 @@ describe('PersonResolvers', () => {
   });
 
   describe('field resolvers', () => {
-    let place: Location;
-    let placeOfBirth: Location;
-    const ctx = getContextForTest();
-    // const person = { ...personAttributes, id, dob: new Date(), dod: new Date() } as Person;
-    const location = createLocationForTest();
-    const person = createPersonForTest(location.id, location.id);
-    // const location = { country: '', city: '' } as Location;
+    describe('locations', () => {
+      let place: Location;
+      let placeOfBirth: Location;
+      const ctx = getContextForTest();
+      const location = createLocationForTest();
+      const person = createPersonForTest(location.id, location.id);
 
-    before(async () => {
-      ctx.placeLoader.load = () => Promise.resolve(location);
-      place = await resolver.place(person, ctx);
-      placeOfBirth = await resolver.placeOfBirth(person, ctx);
+      before(async () => {
+        ctx.placeLoader.load = () => Promise.resolve(location);
+        place = await resolver.place(person, ctx);
+        placeOfBirth = await resolver.placeOfBirth(person, ctx);
+      });
+
+      it('place should return a location', () => {
+        expect(place).toEqual(location);
+      });
+
+      it('placeOfBirth should return a location', () => {
+        expect(placeOfBirth).toEqual(location);
+      });
     });
 
-    it('place should return a location', () => {
-      expect(place).toEqual(location);
-    });
+    describe('family', () => {
+      const family = createFamilyForTest();
+      let getSpousesStub: SinonStub;
 
-    it('placeOfBirth should return a location', () => {
-      expect(placeOfBirth).toEqual(location);
+      before(async () => {
+        Object.keys(family).forEach(async (key: string) =>
+          family[key as keyof typeof family].save(),
+        );
+      });
+
+      after(async () => {
+        Spouse.destroy({ where: {} });
+        Person.destroy({ where: {} });
+        Location.destroy({ where: {} });
+      });
+
+      it('should return spouse for father', async () => {
+        getSpousesStub = stub(personHelpers, 'getSpouses').resolves([family.mother]);
+        const spouses = await resolver.spouses(family.father);
+        getSpousesStub.restore();
+
+        expect(spouses).toEqual([family.mother]);
+      });
+
+      it('should return spouse for mother', async () => {
+        getSpousesStub = stub(personHelpers, 'getSpouses').resolves([family.father]);
+        const spouses = await resolver.spouses(family.mother);
+        getSpousesStub.restore();
+
+        expect(spouses).toEqual([family.father]);
+      });
     });
   });
 });
