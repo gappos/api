36997064adaf2bedc25d61f35b22fd020846463a
Arturas Mickiewicz <arturasmckwcz@gmail.com>
2023-03-16 20:09:08 +0100

    refactored dataloaders

--- src/test/resolvers/context/loaders.test.ts
+++ src/test/resolvers/context/loaders.test.ts
@@ -1,19 +1,24 @@
 import expect from 'expect';
 
-import { LocationResolvers } from '../../../graphql/resolvers/location';
-import { PersonResolvers } from '../../../graphql/resolvers/person';
 import { Location, Person } from '../../../models';
 import { createLocationForTest, createPersonForTest } from '../../utils/utils';
+import { graphqlContext } from '../../../graphql';
 
 describe('context for resolvers', () => {
-  const places = [createLocationForTest(), createLocationForTest()];
+  const placeLiving = createLocationForTest();
+  const placeBorn = createLocationForTest();
   const people = [
-    createPersonForTest(places[0].id, places[1].id),
-    createPersonForTest(places[0].id, places[1].id),
-    createPersonForTest(places[0].id, places[1].id),
+    createPersonForTest(placeLiving.id, placeBorn.id),
+    createPersonForTest(placeLiving.id, placeBorn.id),
+    createPersonForTest(placeLiving.id, placeBorn.id),
   ];
-  const personResolvers = new PersonResolvers();
-  const locationResolvers = new LocationResolvers();
+  const ctx = graphqlContext();
+
+  before(async () => {
+    await placeLiving.save();
+    await placeBorn.save();
+    people.forEach(async (person) => await person.save());
+  });
 
   after(() => {
     Person.destroy({ where: {} });
@@ -21,18 +26,30 @@ describe('context for resolvers', () => {
   });
 
   it('should load correct places for persons', async () => {
-    const persons = await personResolvers.persons();
-    persons.forEach((person) => {
-      expect(person.place).toEqual(places[0]);
-      expect(person.placeOfBirth).toEqual(places[1]);
-    });
+    for (const person of people) {
+      const location = await ctx.placeLoader.load(person.placeId);
+      expect(location.id).toEqual(person.placeId);
+    }
   });
 
-  it('should load correct people for locations', async () => {
-    const locations = await locationResolvers.locations();
-    locations.forEach((location) => {
-      expect(location.personsBorn).toEqual(people);
-      expect(location.personsLiving).toEqual(people);
-    });
+  it('should load correct places of birth for persons', async () => {
+    for (const person of people) {
+      const location = await ctx.placeLoader.load(person.pobId);
+      expect(location.id).toEqual(person.pobId);
+    }
+  });
+
+  it('should load correct people as personsLiving', async () => {
+    const persons = await ctx.peopleLivingLoader.load(placeLiving.id);
+    expect(persons.map(({ dataValues }) => dataValues)).toEqual(
+      people.map(({ dataValues }) => dataValues),
+    );
+  });
+
+  it('should load correct people as personsBorn', async () => {
+    const persons = await ctx.peopleBornLoader.load(placeBorn.id);
+    expect(persons.map(({ dataValues }) => dataValues)).toEqual(
+      people.map(({ dataValues }) => dataValues),
+    );
   });
 });
