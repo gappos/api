969ddeccb69aa0774909d855b41c827ecc710c46
Arturas Mickiewicz <arturasmckwcz@gmail.com>
2023-04-02 23:31:59 +0300

    added add/update mutations

---
+++ src/graphql/resolvers/spouse/index.ts
@@ -0,0 +1,23 @@
+import { Arg, Mutation, Resolver } from 'type-graphql';
+
+import { SpouseInput } from '../types';
+import { addSpouseRelations, updateSpouseRelations } from './helpers';
+import { dsSpouseInput } from '../../../deserialize';
+import { Spouse } from '../../../models';
+
+@Resolver()
+export class SpouseResolvers {
+  @Mutation(() => Boolean)
+  async addSpouse(
+    @Arg('spouseAttributes', () => SpouseInput) spouseAttributes: SpouseInput,
+  ): Promise<Spouse | null> {
+    return addSpouseRelations(dsSpouseInput(spouseAttributes));
+  }
+
+  @Mutation(() => Boolean)
+  async updateSpouse(
+    @Arg('spouseAttributes', () => SpouseInput) spouseAttributes: SpouseInput,
+  ): Promise<Spouse | null> {
+    return updateSpouseRelations(dsSpouseInput(spouseAttributes));
+  }
+}
