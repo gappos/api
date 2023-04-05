29857a75ef64309819d7c921c745d3482339bf7b
Arturas Mickiewicz <arturasmckwcz@gmail.com>
2023-04-05 11:53:11 +0200

    updated and added mutation

--- src/graphql/resolvers/child/helpers.ts
+++ src/graphql/resolvers/child/helpers.ts
@@ -1,5 +1,8 @@
-import { Child, ParentRelation } from '../../../models';
-import { ChildAttributesInput } from '../types';
+import { Child, ChildAttributes, ParentRelation } from '../../../models';
+import { ChildInput, ChildRelationsInput } from '../types';
+import { logger } from '../../../utils';
+
+const log = logger('Child');
 
 export const addChildRelations = async ({
   childId,
@@ -7,17 +10,27 @@ export const addChildRelations = async ({
   parent1relation,
   parent2Id,
   parent2relation,
-}: ChildAttributesInput): Promise<boolean> => {
+}: ChildRelationsInput): Promise<Array<Child | null>> => {
+  const childRelations: Array<Child | null> = [];
+
   if (parent1Id) {
     const relation = parent1relation || ParentRelation.PARENT;
-    await Child.create({ childId, parentId: parent1Id, relation });
+    childRelations.push(await addChild({ childId, parentId: parent1Id, relation }));
   }
 
   if (parent2Id) {
     const relation = parent2relation || ParentRelation.PARENT;
-    await Child.create({ childId, parentId: parent2Id, relation });
+    childRelations.push(await addChild({ childId, parentId: parent2Id, relation }));
   }
 
-  if (parent1Id || parent2Id) return true;
-  return false;
+  return childRelations;
+};
+
+export const addChild = async (childAttributes: ChildInput): Promise<Child | null> => {
+  try {
+    return await Child.create(childAttributes as ChildAttributes);
+  } catch (error) {
+    log.error('create', (error as Error).message);
+  }
+  return null;
 };
--- src/graphql/resolvers/child/index.ts
+++ src/graphql/resolvers/child/index.ts
@@ -1,14 +1,23 @@
 import { Arg, Mutation, Resolver } from 'type-graphql';
 
-import { ChildAttributesInput } from '../types';
-import { addChildRelations } from './helpers';
+import { ChildInput, ChildRelationsInput } from '../types';
+import { addChild, addChildRelations } from './helpers';
+import { Child } from '../../../models';
 
 @Resolver()
 export class ChildResolvers {
-  @Mutation(() => Boolean)
+  @Mutation(() => Child)
   async addChild(
-    @Arg('childAttributes', () => ChildAttributesInput) childAttributes: ChildAttributesInput,
-  ): Promise<boolean> {
-    return addChildRelations(childAttributes);
+    @Arg('childAttributes', () => ChildInput) childAttributes: ChildInput,
+  ): Promise<Child | null> {
+    return addChild(childAttributes);
+  }
+
+  @Mutation(() => Boolean)
+  async addChildRelations(
+    @Arg('childRelationsAttributes', () => ChildRelationsInput)
+    childRelationsAttributes: ChildRelationsInput,
+  ): Promise<Array<Child | null>> {
+    return addChildRelations(childRelationsAttributes);
   }
 }
