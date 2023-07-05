d686a10505705b327a3e6929007463b8eb2d04d3
Arturas Mickiewicz <arturasmckwcz@gmail.com>
2023-07-05 13:29:11 +0300

    added isEmpty util

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,6 @@
 export * from './logger';
-export * from './isEmpty';
+
+export const isEmpty = (obj: object) => Object.keys(obj).length === 0;
+
+export const range = (length: number, from = 0) =>
+  new Array(length).fill(undefined).map((_, idx) => idx + from);
