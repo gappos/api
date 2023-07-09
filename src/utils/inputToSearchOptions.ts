a5b2b9ed3558eb82784e5ef38034f12f1d1e908e
Arturas Mickiewicz <arturasmckwcz@gmail.com>
2023-07-09 19:01:31 +0200

    added processing of date fields

--- src/utils/inputToSearchOptions.ts
+++ src/utils/inputToSearchOptions.ts
@@ -1,4 +1,5 @@
 import { Op } from 'sequelize';
+import { logger } from './logger';
 
 export type SearchOptions = Record<string, unknown>;
 export interface InputToSearchOptions {
@@ -9,7 +10,36 @@ export const inputToSearchOptions = (inputObj: SearchOptions, options?: InputToS
   Object.keys(inputObj ?? {}).reduce((searchObj: SearchOptions, key) => {
     if (options?.equalKeys && options.equalKeys.includes(key)) searchObj[key] = inputObj[key];
     else if (options?.dateKeys && options.dateKeys.includes(key))
-      searchObj[key] = inputObj[key]; // TODO: logic for date fields
+      searchObj[key] = getDateRangeSearchObj(inputObj[key] as string);
     else searchObj[key] = { [Op.iLike]: inputObj[key] };
     return searchObj;
   }, {});
+
+export const getDateRangeSearchObj = (date: string) => {
+  const getLastDay = (date: string): Date => {
+    const [year, month, day] = date.split('-');
+    return day ? new Date(date) : new Date(+year, month ? +month : 12, 0);
+  };
+
+  const [from, to] = date.split(':');
+  const dateFrom = new Date(from);
+  const dateTo = new Date(to);
+
+  if (
+    dateFrom.toString() === 'Invalid Date' ||
+    (to !== undefined && dateTo.toString() === 'Invalid Date')
+  ) {
+    logger('utils').error(
+      'getDateRangeSearchObj()',
+      'got wrong date, correct usage is from:to like "2000:2020-02-02"',
+    );
+    return {};
+  }
+
+  const [, , dayFrom] = from.split('-');
+  if (!!dayFrom && !to) return new Date(dateFrom);
+
+  const dateSearchTo = !!to ? getLastDay(to) : getLastDay(from);
+
+  return { [Op.between]: [dateFrom, dateSearchTo] };
+};
